921f2ef64a2d5177b479ebea4f5db24c30fede42
Antoine Pavy <antoine.pavy92@gmail.com>
2022-03-15 12:41:32 +0100

    UE4 TPS Demo project added

---
+++ Source/MarioGalaxyTutorial/MarioGalaxyTutorialGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MarioGalaxyTutorialGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMarioGalaxyTutorialGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMarioGalaxyTutorialGameMode();
+};
+
+
+
